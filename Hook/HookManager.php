fe9d530e0386f230449764a32965e7bc98f2a82b
Franck Allimant <github@allimant.org>
2016-06-15 18:51:23 +0200

    Fixed file headers

--- Hook/HookManager.php
+++ Hook/HookManager.php
@@ -1,11 +1,13 @@
 <?php
 /*************************************************************************************/
+/*                                                                                   */
 /*      Copyright (c) Franck Allimant, CQFDev                                        */
 /*      email : thelia@cqfdev.fr                                                     */
 /*      web : http://www.cqfdev.fr                                                   */
 /*                                                                                   */
-/*      For the full copyright and license information, please view the LICENSE.txt  */
+/*      For the full copyright and license information, please view the LICENSE      */
 /*      file that was distributed with this source code.                             */
+/*                                                                                   */
 /*************************************************************************************/
 
 /**
